f238d1022ca76049ac702972c0b5140c8ed05119
tom <tom@zhiming.org>
2020-03-02 08:57:37 +0800

    fix flag bug.

--- gonews/back/main.go
+++ gonews/back/main.go
@@ -9,6 +9,7 @@ import (
 var configFileName = flag.String("c", "config.ini", "config file path (default config.ini)")
 
 func main() {
+	flag.Parse()
 	config, _ := common.InitConfig(*configFileName)
 	common.InitEnv(config)
 	common.StartServ(config)
